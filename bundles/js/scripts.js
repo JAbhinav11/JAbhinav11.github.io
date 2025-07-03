ac76578ca36c9ee6cdc4adf27ab05d44b69a80c1
Abhinav <Av9Abhinav@gmail.com>
2025-07-03 14:09:11 +0530

    Adding webp image

--- bundles/js/scripts.js
+++ bundles/js/scripts.js
@@ -4,295 +4,296 @@
 const elementToggleFunc = function (elem) {
   elem.classList.toggle("active");
 };
-const frameworksLanguagesExpertiseButtons = [
-  {
-    name: "Flutter",
-    icon: "./bundles/images/svg/flutter.svg",
-  },
-  {
-    name: "Dart",
-    icon: "./bundles/images/svg/dart.svg",
-  },
-  {
-    name: "FlutterFlow",
-    icon: "./bundles/images/svg/flutterflow.svg",
-  },
-  {
-    name: "Android Native",
-    icon: "./bundles/images/svg/android.svg",
-  },
-  {
-    name: "Kotlin",
-    icon: "./bundles/images/svg/kotlin.svg",
-  },
-  {
-    name: "iOS Native",
-    icon: "./bundles/images/svg/iOS.png",
-  },
-  {
-    name: "TypeScript",
-    icon: "./bundles/images/svg/typeScript.svg",
-  },
-  {
-    name: "JavaScript",
-    icon: "./bundles/images/svg/javascript.svg",
-  },
-];
 
-const paymentGatewayExpertiseButtons = [
-  {
-    name: "Razorpay",
-    icon: "./bundles/images/svg/razorpay.png",
-  },
-  {
-    name: "Stripe",
-    icon: "./bundles/images/svg/stripe.svg",
-  },
-  {
-    name: "In App Purchase",
-    icon: "./bundles/images/svg/iap.svg",
-  },
-  {
-    name: "Braintree",
-    icon: "./bundles/images/svg/braintree.svg",
-  },
-  {
-    name: "Google Pay",
-    icon: "./bundles/images/svg/google-pay.svg",
-  },
-];
-const apiServicesExpertiseButtons = [
-  {
-    name: "REST API",
-    icon: "./bundles/images/svg/api.svg",
-  },
-  {
-    name: "GraphQL",
-    icon: "./bundles/images/svg/graphql.svg",
-  },
-];
-const uiUXToolsExpertiseButtons = [
-  {
-    name: "Figma",
-    icon: "./bundles/images/svg/figma.svg",
-  },
-  {
-    name: "Framer",
-    icon: "./bundles/images/svg/framer.svg",
-  },
-  {
-    name: "Whimsical",
-    icon: "./bundles/images/svg/whimsical.jpg",
-  },
-];
-const unitTestingExpertiseButtons = [
-  {
-    name: "Widget Tests",
-    icon: "./bundles/images/svg/widget.png",
-  },
-  {
-    name: "Unit Tests",
-    icon: "./bundles/images/svg/unit-testing.png",
-  },
-  {
-    name: "Integration Tests",
-    icon: "./bundles/images/svg/integration-testing.webp",
-  },
-];
-const upgradeServicesExpertiseButtons = [
-  {
-    name: "Puro",
-    icon: "./bundles/images/svg/puro.png",
-  },
-  {
-    name: "FVM",
-    icon: "./bundles/images/svg/fvm.svg",
-  },
-];
-const deploymentProcessExpertiseButtons = [
-  "Cirrus",
-  "Travis",
-  "Codemagic",
-  "Bitrise",
-];
-const stateManagementExpertiseButtons = [
-  {
-    name: "Provider",
-    icon: "./bundles/images/svg/provider.png",
-  },
-  {
-    name: "BLoC",
-    icon: "./bundles/images/svg/bloc.png",
-  },
-  {
-    name: "GetX",
-    icon: "./bundles/images/svg/getx.png",
-  },
-  {
-    name: "Riverpod",
-    icon: "./bundles/images/svg/riverpod.svg",
-  },
-];
-const projectManagementToolsExpertiseButtons = [
-  {
-    name: "asana",
-    icon: "./bundles/images/svg/asana.svg",
-  },
-  {
-    name: "Slack",
-    icon: "./bundles/images/svg/slack.svg",
-  },
-  {
-    name: "Jira",
-    icon: "./bundles/images/svg/jira.svg",
-  },
-  {
-    name: "Trello",
-    icon: "./bundles/images/svg/trello.svg",
-  },
-];
-const crashReportingExpertiseButtons = [
-  {
-    name: "Firebase Crashlytics",
-    icon: "./bundles/images/svg/firebase-crashlytics.png",
-  },
-  {
-    name: "Sentry",
-    icon: "./bundles/images/svg/sentry.svg",
-  },
-];
-const deploymentExpertiseButtons = [
-  {
-    name: "Google Play Stroe",
-    icon: "./bundles/images/svg/google-play.png",
-  },
-  {
-    name: "App Stroe",
-    icon: "./bundles/images/svg/app-store.svg",
-  },
-  {
-    name: "Web",
-    icon: "./bundles/images/svg/web.svg",
-  },
-  {
-    name: "Microsoft Store",
-    icon: "./bundles/images/svg/microsoft-store.png",
-  },
-  {
-    name: "Linux",
-    icon: "./bundles/images/svg/linux.svg",
-  },
-];
-const codeReviewExpertiseButtons = [
-  {
-    name: "Flutter Analyze",
-    icon: "./bundles/images/svg/flutter-analyze.svg",
-  },
-  {
-    name: "Effective Dart-Default IDE Static Analysis",
-    icon: "./bundles/images/svg/code-Effective.png",
-  },
-  {
-    name: "pana",
-    icon: "./bundles/images/svg/pana.png",
-  },
-];
-const databaseExpertiseButtons = [
-  {
-    name: "Local Storage",
-    icon: "./bundles/images/svg/local-database.png",
-  },
-  {
-    name: "Sqlite",
-    icon: "./bundles/images/svg/sqlite.svg",
-  },
-  {
-    name: "Firebase Firestore",
-    icon: "./bundles/images/svg/firebase-firestore.png",
-  },
-  {
-    name: "Firebase realtime database",
-    icon: "./bundles/images/svg/firebase-realtime.png",
-  },
-  {
-    name: "Supabase",
-    icon: "./bundles/images/svg/supabase.svg",
-  },
-  {
-    name: "Hive",
-    icon: "./bundles/images/svg/hive.png",
-  },
-  {
-    name: "ObjectBox",
-    icon: "./bundles/images/svg/objectbox.png",
-  },
-];
-const versionControlExpertiseButtons = [
-  {
-    name: "Git",
-    icon: "./bundles/images/svg/git.svg",
-  },
-  {
-    name: "Github",
-    icon: "./bundles/images/svg/github.svg",
-  },
-  {
-    name: "BitBucket",
-    icon: "./bundles/images/svg/bitbucket.svg",
-  },
-];
-const tootlUtilitiesExpertiseButtons = [
-  {
-    name: "Android Studio",
-    icon: "./bundles/images/svg/android-studio.svg",
-  },
-  {
-    name: "Visual Studio Code",
-    icon: "./bundles/images/svg/visual-studio-code.svg",
-  },
-  {
-    name: "XCode",
-    icon: "./bundles/images/svg/xcode.svg",
-  },
-  {
-    name: "Postman",
-    icon: "./bundles/images/svg/postman-icon.svg",
-  },
-];
-const plugnisExpertiseButtons = [
-  {
-    name: "Firebase",
-    icon: "./bundles/images/svg/firebase.svg",
-  },
-  {
-    name: "OneSignal Notifications",
-    icon: "./bundles/images/svg/onesignal.svg",
-  },
-  {
-    name: "In-App-Purchase",
-    icon: "./bundles/images/svg/iap.svg",
-  },
-  {
-    name: "Google Map",
-    icon: "./bundles/images/svg/google-map.svg",
-  },
-  {
-    name: "Social Login",
-    icon: "./bundles/images/svg/social-login.png",
-  },
-  {
-    name: "Notifications",
-    icon: "./bundles/images/svg/notification.png",
-  },
-  {
-    name: "Injectable",
-    icon: "./bundles/images/svg/injectable.svg",
-  },
-  {
-    name: "Json Serializable",
-    icon: "./bundles/images/svg/json.svg",
-  },
-];
+//const frameworksLanguagesExpertiseButtons = [
+//  {
+//    name: "Flutter",
+//    icon: "./bundles/images/svg/flutter.svg",
+//  },
+//  {
+//    name: "Dart",
+//    icon: "./bundles/images/svg/dart.svg",
+//  },
+//  {
+//    name: "FlutterFlow",
+//    icon: "./bundles/images/svg/flutterflow.svg",
+//  },
+//  {
+//    name: "Android Native",
+//    icon: "./bundles/images/svg/android.svg",
+//  },
+//  {
+//    name: "Kotlin",
+//    icon: "./bundles/images/svg/kotlin.svg",
+//  },
+//  {
+//    name: "iOS Native",
+//    icon: "./bundles/images/svg/iOS.png",
+//  },
+//  {
+//    name: "TypeScript",
+//    icon: "./bundles/images/svg/typeScript.svg",
+//  },
+//  {
+//    name: "JavaScript",
+//    icon: "./bundles/images/svg/javascript.svg",
+//  },
+//];
+//
+//const paymentGatewayExpertiseButtons = [
+//  {
+//    name: "Razorpay",
+//    icon: "./bundles/images/svg/razorpay.png",
+//  },
+//  {
+//    name: "Stripe",
+//    icon: "./bundles/images/svg/stripe.svg",
+//  },
+//  {
+//    name: "In App Purchase",
+//    icon: "./bundles/images/svg/iap.svg",
+//  },
+//  {
+//    name: "Braintree",
+//    icon: "./bundles/images/svg/braintree.svg",
+//  },
+//  {
+//    name: "Google Pay",
+//    icon: "./bundles/images/svg/google-pay.svg",
+//  },
+//];
+//const apiServicesExpertiseButtons = [
+//  {
+//    name: "REST API",
+//    icon: "./bundles/images/svg/api.svg",
+//  },
+//  {
+//    name: "GraphQL",
+//    icon: "./bundles/images/svg/graphql.svg",
+//  },
+//];
+//const uiUXToolsExpertiseButtons = [
+//  {
+//    name: "Figma",
+//    icon: "./bundles/images/svg/figma.svg",
+//  },
+//  {
+//    name: "Framer",
+//    icon: "./bundles/images/svg/framer.svg",
+//  },
+//  {
+//    name: "Whimsical",
+//    icon: "./bundles/images/svg/whimsical.jpg",
+//  },
+//];
+//const unitTestingExpertiseButtons = [
+//  {
+//    name: "Widget Tests",
+//    icon: "./bundles/images/svg/widget.png",
+//  },
+//  {
+//    name: "Unit Tests",
+//    icon: "./bundles/images/svg/unit-testing.png",
+//  },
+//  {
+//    name: "Integration Tests",
+//    icon: "./bundles/images/svg/integration-testing.webp",
+//  },
+//];
+//const upgradeServicesExpertiseButtons = [
+//  {
+//    name: "Puro",
+//    icon: "./bundles/images/svg/puro.png",
+//  },
+//  {
+//    name: "FVM",
+//    icon: "./bundles/images/svg/fvm.svg",
+//  },
+//];
+//const deploymentProcessExpertiseButtons = [
+//  "Cirrus",
+//  "Travis",
+//  "Codemagic",
+//  "Bitrise",
+//];
+//const stateManagementExpertiseButtons = [
+//  {
+//    name: "Provider",
+//    icon: "./bundles/images/svg/provider.png",
+//  },
+//  {
+//    name: "BLoC",
+//    icon: "./bundles/images/svg/bloc.png",
+//  },
+//  {
+//    name: "GetX",
+//    icon: "./bundles/images/svg/getx.png",
+//  },
+//  {
+//    name: "Riverpod",
+//    icon: "./bundles/images/svg/riverpod.svg",
+//  },
+//];
+//const projectManagementToolsExpertiseButtons = [
+//  {
+//    name: "asana",
+//    icon: "./bundles/images/svg/asana.svg",
+//  },
+//  {
+//    name: "Slack",
+//    icon: "./bundles/images/svg/slack.svg",
+//  },
+//  {
+//    name: "Jira",
+//    icon: "./bundles/images/svg/jira.svg",
+//  },
+//  {
+//    name: "Trello",
+//    icon: "./bundles/images/svg/trello.svg",
+//  },
+//];
+//const crashReportingExpertiseButtons = [
+//  {
+//    name: "Firebase Crashlytics",
+//    icon: "./bundles/images/svg/firebase-crashlytics.png",
+//  },
+//  {
+//    name: "Sentry",
+//    icon: "./bundles/images/svg/sentry.svg",
+//  },
+//];
+//const deploymentExpertiseButtons = [
+//  {
+//    name: "Google Play Stroe",
+//    icon: "./bundles/images/svg/google-play.png",
+//  },
+//  {
+//    name: "App Stroe",
+//    icon: "./bundles/images/svg/app-store.svg",
+//  },
+//  {
+//    name: "Web",
+//    icon: "./bundles/images/svg/web.svg",
+//  },
+//  {
+//    name: "Microsoft Store",
+//    icon: "./bundles/images/svg/microsoft-store.png",
+//  },
+//  {
+//    name: "Linux",
+//    icon: "./bundles/images/svg/linux.svg",
+//  },
+//];
+//const codeReviewExpertiseButtons = [
+//  {
+//    name: "Flutter Analyze",
+//    icon: "./bundles/images/svg/flutter-analyze.svg",
+//  },
+//  {
+//    name: "Effective Dart-Default IDE Static Analysis",
+//    icon: "./bundles/images/svg/code-Effective.png",
+//  },
+//  {
+//    name: "pana",
+//    icon: "./bundles/images/svg/pana.png",
+//  },
+//];
+//const databaseExpertiseButtons = [
+//  {
+//    name: "Local Storage",
+//    icon: "./bundles/images/svg/local-database.png",
+//  },
+//  {
+//    name: "Sqlite",
+//    icon: "./bundles/images/svg/sqlite.svg",
+//  },
+//  {
+//    name: "Firebase Firestore",
+//    icon: "./bundles/images/svg/firebase-firestore.png",
+//  },
+//  {
+//    name: "Firebase realtime database",
+//    icon: "./bundles/images/svg/firebase-realtime.png",
+//  },
+//  {
+//    name: "Supabase",
+//    icon: "./bundles/images/svg/supabase.svg",
+//  },
+//  {
+//    name: "Hive",
+//    icon: "./bundles/images/svg/hive.png",
+//  },
+//  {
+//    name: "ObjectBox",
+//    icon: "./bundles/images/svg/objectbox.png",
+//  },
+//];
+//const versionControlExpertiseButtons = [
+//  {
+//    name: "Git",
+//    icon: "./bundles/images/svg/git.svg",
+//  },
+//  {
+//    name: "Github",
+//    icon: "./bundles/images/svg/github.svg",
+//  },
+//  {
+//    name: "BitBucket",
+//    icon: "./bundles/images/svg/bitbucket.svg",
+//  },
+//];
+//const tootlUtilitiesExpertiseButtons = [
+//  {
+//    name: "Android Studio",
+//    icon: "./bundles/images/svg/android-studio.svg",
+//  },
+//  {
+//    name: "Visual Studio Code",
+//    icon: "./bundles/images/svg/visual-studio-code.svg",
+//  },
+//  {
+//    name: "XCode",
+//    icon: "./bundles/images/svg/xcode.svg",
+//  },
+//  {
+//    name: "Postman",
+//    icon: "./bundles/images/svg/postman-icon.svg",
+//  },
+//];
+//const plugnisExpertiseButtons = [
+//  {
+//    name: "Firebase",
+//    icon: "./bundles/images/svg/firebase.svg",
+//  },
+//  {
+//    name: "OneSignal Notifications",
+//    icon: "./bundles/images/svg/onesignal.svg",
+//  },
+//  {
+//    name: "In-App-Purchase",
+//    icon: "./bundles/images/svg/iap.svg",
+//  },
+//  {
+//    name: "Google Map",
+//    icon: "./bundles/images/svg/google-map.svg",
+//  },
+//  {
+//    name: "Social Login",
+//    icon: "./bundles/images/svg/social-login.png",
+//  },
+//  {
+//    name: "Notifications",
+//    icon: "./bundles/images/svg/notification.png",
+//  },
+//  {
+//    name: "Injectable",
+//    icon: "./bundles/images/svg/injectable.svg",
+//  },
+//  {
+//    name: "Json Serializable",
+//    icon: "./bundles/images/svg/json.svg",
+//  },
+//];
 
 if ('scrollRestoration' in history) {
   history.scrollRestoration = 'manual';
@@ -452,23 +453,23 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-function createButtonListWithIcon(buttonNames, containerId) {
-  const container = document.getElementById(containerId);
-  container.innerHTML = "";
-
-  buttonNames.forEach((item) => {
-    const button = document.createElement("button");
-    const icon = document.createElement("img"); // Use img instead of span
-    icon.className = "icon";
-    icon.src = item.icon;
-    icon.alt = `${item.name} icon`;
-
-    button.className = "toolBtn";
-    button.textContent = item.name;
-    button.appendChild(icon);
-    container.appendChild(button);
-  });
-}
+//function createButtonListWithIcon(buttonNames, containerId) {
+//  const container = document.getElementById(containerId);
+//  container.innerHTML = "";
+//
+//  buttonNames.forEach((item) => {
+//    const button = document.createElement("button");
+//    const icon = document.createElement("img"); // Use img instead of span
+//    icon.className = "icon";
+//    icon.src = item.icon;
+//    icon.alt = `${item.name} icon`;
+//
+//    button.className = "toolBtn";
+//    button.textContent = item.name;
+//    button.appendChild(icon);
+//    container.appendChild(button);
+//  });
+//}
 
 // calculate years of experience
 const startYear = 2018;
@@ -493,7 +494,7 @@ const popupImg = popup.querySelector("img");
 document.addEventListener("click", function (e) {
   const target = e.target;
   if (target.matches("[data-popup-image]")) {
-    const src = target.getAttribute("src");
+    const src = target.currentSrc;
     popupImg.setAttribute("src", src);
     popup.classList.add("show");
   }
@@ -656,45 +657,45 @@ fetch('/bundles/files/cards.json')
     });
   }
 
-// Call the function with different lists and container IDs
-createButtonListWithIcon(
-  frameworksLanguagesExpertiseButtons,
-  "frameworksLanguagesExpertise"
-);
-
-createButtonListWithIcon(
-  paymentGatewayExpertiseButtons,
-  "paymentGatewayExpertise"
-);
-createButtonListWithIcon(plugnisExpertiseButtons, "plugnisExpertise");
-createButtonListWithIcon(
-  tootlUtilitiesExpertiseButtons,
-  "tootlUtilitiesExpertise"
-);
-createButtonListWithIcon(
-  versionControlExpertiseButtons,
-  "versionControlExpertise"
-);
-createButtonListWithIcon(databaseExpertiseButtons, "databaseExpertise");
-createButtonListWithIcon(apiServicesExpertiseButtons, "apiServicesExpertise");
-createButtonListWithIcon(codeReviewExpertiseButtons, "codeReviewExpertise");
-createButtonListWithIcon(deploymentExpertiseButtons, "deploymentExpertise");
-createButtonListWithIcon(
-  crashReportingExpertiseButtons,
-  "crashReportingExpertise"
-);
-createButtonListWithIcon(
-  projectManagementToolsExpertiseButtons,
-  "projectManagementToolsExpertise"
-);
-
-createButtonListWithIcon(
-  stateManagementExpertiseButtons,
-  "stateManagementExpertise"
-);
-createButtonListWithIcon(
-  upgradeServicesExpertiseButtons,
-  "upgradeServicesExpertise"
-);
-createButtonListWithIcon(unitTestingExpertiseButtons, "unitTestingExpertise");
-createButtonListWithIcon(uiUXToolsExpertiseButtons, "uiUXToolsExpertise");
+//// Call the function with different lists and container IDs
+//createButtonListWithIcon(
+//  frameworksLanguagesExpertiseButtons,
+//  "frameworksLanguagesExpertise"
+//);
+//
+//createButtonListWithIcon(
+//  paymentGatewayExpertiseButtons,
+//  "paymentGatewayExpertise"
+//);
+//createButtonListWithIcon(plugnisExpertiseButtons, "plugnisExpertise");
+//createButtonListWithIcon(
+//  tootlUtilitiesExpertiseButtons,
+//  "tootlUtilitiesExpertise"
+//);
+//createButtonListWithIcon(
+//  versionControlExpertiseButtons,
+//  "versionControlExpertise"
+//);
+//createButtonListWithIcon(databaseExpertiseButtons, "databaseExpertise");
+//createButtonListWithIcon(apiServicesExpertiseButtons, "apiServicesExpertise");
+//createButtonListWithIcon(codeReviewExpertiseButtons, "codeReviewExpertise");
+//createButtonListWithIcon(deploymentExpertiseButtons, "deploymentExpertise");
+//createButtonListWithIcon(
+//  crashReportingExpertiseButtons,
+//  "crashReportingExpertise"
+//);
+//createButtonListWithIcon(
+//  projectManagementToolsExpertiseButtons,
+//  "projectManagementToolsExpertise"
+//);
+//
+//createButtonListWithIcon(
+//  stateManagementExpertiseButtons,
+//  "stateManagementExpertise"
+//);
+//createButtonListWithIcon(
+//  upgradeServicesExpertiseButtons,
+//  "upgradeServicesExpertise"
+//);
+//createButtonListWithIcon(unitTestingExpertiseButtons, "unitTestingExpertise");
+//createButtonListWithIcon(uiUXToolsExpertiseButtons, "uiUXToolsExpertise");
